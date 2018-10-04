babf6f3ffe2a4340aed2176dba5094363a17b3ee
Shanky Rana <shankyrana@hotmail.com>
2018-10-04 23:18:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var app = express();
 //app.set('views', path.join(__dirname, 'views'));
 //app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'html');
-//app.use(logger('dev'));
+//app.use(logger('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
